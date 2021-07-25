b1c9f970a669ced5c8e9a710c7372c8c9114ef63
BeJ Park <bjpark0506@naver.com>
2021-07-25 19:17:54 +0900

    [ADD] FavoritePage, Actor.. Err..

---
+++ boilerplate-mern-stack-master/client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios'
+
+function Favorite(props) {
+    const movieId = props.movieId
+    const userForm = props.userForm
+    const movieTitle = props.movieInfo.title
+    const moviePost = props.movieInfo.backdrop_path
+    const movieRunTime = props.movieInfo.runtime
+    const [FavoriteNumber, setFavoriteNumber] = useState(0)
+    const [Favorited, setFavorited] = useState(false)
+    let variables = {
+        userForm:userForm, 
+        movieId:movieId,
+        movieTitle:movieTitle,
+        moviePost:moviePost,
+        movieRunTime:movieRunTime
+    }
+    useEffect(() => {
+        Axios.post('/api/favorite/favoriteNumber', variables)
+            .then(response => {
+                if (response.data.success) {
+                    setFavoriteNumber(response.data.favoriteNumber)
+                } else {
+                    alert('숫자 정보를 가져오는데 실패했습니다.')
+                }
+            })
+
+        Axios.post('/api/favorite/favorited', variables)
+            .then(response => {
+                if (response.data.success) {
+                    setFavorited(response.data.favorited)
+                } else {
+                    alert('정보를 가져오는데 실패했습니다.')
+                }
+            })
+    }, [])
+
+    const onClickFavorite=()=>{
+        if(Favorited){
+            Axios.post('/api/favorite/removeFromFavorite',variables)
+            .then(response=>{
+                if(response.data.success){
+                    setFavoriteNumber(FavoriteNumber-1)
+                    setFavorited(!Favorited)
+                }else{
+                    alert('Favorite 리스트에서 지우는 것을 실패했습니다.')
+                }
+            })
+        }else{
+            Axios.post('/api/favorite/addFavorite',variables)
+            .then(response=>{
+                if(response.data.success){
+                    setFavoriteNumber(FavoriteNumber+1)
+                    setFavorited(!Favorited)
+                }else{
+                    alert('Favorite 리스트에서 추가하는 것을 실패했습니다.')
+                }
+            })
+
+        }
+    }
+    return ( 
+        <div>
+            <button onClick={onClickFavorite}>{Favorited? "Not Favorite" : "Add to Favorite"}{FavoriteNumber}</button>
+        </div>
+
+    )
+}
+
+export default Favorite
